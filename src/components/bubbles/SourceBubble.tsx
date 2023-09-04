1122d286d559d1d0a974a0ba2cc0cb32524c5cf6
David Riha <kontakt@rihadavid.cz>
2023-09-04 22:04:43 +0200

    source bubble tests

--- src/components/bubbles/SourceBubble.tsx
+++ src/components/bubbles/SourceBubble.tsx
@@ -1,9 +1,9 @@
 type Props = {
-  pageContent: string
-  metadata: object
-  onSourceClick?: () => void
+    pageContent: string
+    metadata: object
+    onSourceClick?: () => void
 }
-export const SourceBubble = (props: Props) => (
+/*export const SourceBubble = (props: Props) => (
   <>
     <div
       data-modal-target="defaultModal" data-modal-toggle="defaultModal"
@@ -19,4 +19,36 @@ export const SourceBubble = (props: Props) => (
       </span>
     </div>
   </>
+)
+*/
+export const SourceBubble = (props: Props) => (
+    <>
+        <div
+            data-modal-target="defaultModal" data-modal-toggle="defaultModal"
+            class="flex justify-start mb-2 items-start animate-fade-in host-container hover:brightness-90 active:brightness-75"
+            onClick={() =>{
+
+                let newWindow = window.open(props.metadata['sourceUrl'], "_blank");
+                if (newWindow)
+                    newWindow.focus();
+            }}
+        >
+      <span
+          class="px-2 py-1 ml-1 whitespace-pre-wrap max-w-full chatbot-host-bubble"
+          data-testid="host-bubble"
+          style={{
+              width: 'max-content',
+              "max-width": '80px',
+              "font-size": "13px",
+              "border-radius": '15px',
+              cursor: 'pointer',
+              "text-overflow": "ellipsis",
+              "overflow": "hidden",
+              "white-space": "nowrap"
+          }}
+      >
+        {props.metadata['sourceName']}
+      </span>
+        </div>
+    </>
 )
